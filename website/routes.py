6e4aeb0e2d4305e73ee65075fe27c152bb30e36f
dexdreamin <dexterkhoowc@gmail.com>
2023-02-15 02:04:25 +0800

    committing something rn

--- website/routes.py
+++ website/routes.py
@@ -2479,7 +2479,7 @@ def register_staff_account():
             flash(
                 f"Success! Account {user_to_create.username} created!", category='success')
 
-            return redirect(url_for('landing_page'))
+            return redirect(url_for('register_staff_account'))
 
     return render_template('registerStaffAccount.html', form=form, staff=staff)
 
@@ -2809,11 +2809,21 @@ def register_page():
 @app.route('/logout')
 @login_required
 def logout_page():
-    logout_user()
     # to log out the current logged in user
     # the category for flash will decide the color of the flashed message
     # for instance 'info' is blue, 'danger' is red, 'success' is green.
-    return redirect(url_for("landing_page"))
+    if current_user.usertype == "customers":
+        logout_user()
+        flash("You have been logged out!", category='info')
+        return redirect(url_for("landing_page"))
+    elif current_user.admin == 1:
+        logout_user()
+        flash("You have been logged out!", category='info')
+        return redirect(url_for("landing_pagea"))
+    else:
+        logout_user()
+        flash("You have been logged out!", category='info')
+        return redirect(url_for("pro_login"))
     # flash("You have been logged out!", category='info')
     # redirects user to login page after they are logged out.
 
@@ -2927,7 +2937,7 @@ def landing_page():
                     return redirect(url_for('home_page'))
                 else:
                     logout_user()
-                    flash(f"If you are not a customer, please login to the login page for retailers, staff and admin. ", category='danger')
+                    flash(f"If you are not a customer, please login to the professional login page side for retailers and staff. ", category='danger')
                     return redirect(url_for('landing_page'))
 
             else:
@@ -2956,6 +2966,39 @@ def landing_pagea():
             elif attempted_user.account_availability(attempted_user.status) != 0:
                 # checks username for valid user and checks if password is correct
                 login_user(attempted_user)
+                if current_user.admin == 1:
+                    # 'login_user' is a built-in function for flask_login
+                    flash(f"Success! You are logged in as: {attempted_user.username}", category='success')
+                    return redirect(url_for('home_page'))
+                else:
+                    logout_user()
+                    flash(f"This login page is for admins. ", category='danger')
+                    return redirect(url_for('landing_pagea'))
+            else:
+                flash(f"{attempted_user.username} account has been disabled!"
+                      f" Please contact Customer Support for more information.", category='danger')
+        else:
+            flash("Username or Password are not matched! Please try again.", category='danger')
+    
+    return render_template('LandingB.html', form=form)
+
+@app.route('/professional', methods=["GET", "POST"])
+def pro_login():
+    admin_user()
+
+    db.create_all()
+    # warning very funny error when logging in if passwords are not hashed(check SQlite) it will crash
+    # giving an error of Invalid salt Value error
+    form = LoginForm()
+    if form.validate_on_submit():
+        # if user exist and if password is correct
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            if attempted_user.account_availability(attempted_user.status) == "sven":
+                return redirect(url_for('home_page'))
+            elif attempted_user.account_availability(attempted_user.status) != 0:
+                # checks username for valid user and checks if password is correct
+                login_user(attempted_user)
                 if current_user.usertype != 'customers':
                     # 'login_user' is a built-in function for flask_login
                     flash(f"Success! You are logged in as: {attempted_user.username}", category='success')
@@ -2966,15 +3009,15 @@ def landing_pagea():
                 else:
                     logout_user()
                     flash(f"If you are a customer, please login to the customer login page side. ", category='danger')
-                    return redirect(url_for('landing_pagea'))
+                    return redirect(url_for('pro_login'))
             else:
                 flash(f"{attempted_user.username} account has been disabled!"
                       f" Please contact Customer Support for more information.", category='danger')
         else:
-            flash("Username or Password are not matched! Please try again.", category='danger')
-    
-    return render_template('LandingB.html', form=form)
+            flash("Username or Password are not matched! Please try again.",
+                  category='danger')
 
+    return render_template('professional_login.html', form=form)
 
 
 
