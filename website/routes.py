c9861cef3bdb845e7acfebbbd42224f704e2ec96
iamsven2005 <108319539+iamsven2005@users.noreply.github.com>
2023-02-16 12:17:12 +0800

    bruh

--- website/routes.py
+++ website/routes.py
@@ -1234,8 +1234,8 @@ def remove_from_cart():
 @login_required
 def Receipt():
     def createQR(*args: Item):
-        return qrcode.make('Receipt:\n{}\nTotal price:${}'.format('\n'.join(
-            [i.get_name() + ',Qty: ' + str(i.get_qty_purchased()) + ',Cost: $' + str(i.get_total_cost()) + ',OrderDate: Jan 8 2023, warranty for 1 year, End-Date: Jan 8 2024' for i in
+        return qrcode.make('www.google.com/Receipt:\n{}\nTotal price:${}'.format('\n'.join(
+            [i.get_name() + ',Qty: ' + str(i.get_qty_purchased()) + ',Cost: $' + str(i.get_total_cost()) + ',OrderDate: Feb 16 2023, warranty for 1 year, End-Date: Feb 16 2024' for i in
              args]),
             sum([i.get_total_cost() for i in args])))
 
